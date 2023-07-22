f511b36c48d5325547068960a59ef988ccd6069f
system3600 <127296307+system3600@users.noreply.github.com>
2023-07-22 15:27:02 -0300

    Event Update
    visit the changelog for more info !!

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     if mode == 2:
         if lang == 1:
             print('A linguagem atual é : PT-BR')
-            opr = int(input('Selecione a operação \n1)auto egg open \n2)anti-afk \n3)auto click \n4)Sobre \n5)Sair\n> '))
+            opr = int(input('Selecione a operação \n1)auto egg open \n2)anti-afk \n3)auto click \n4)event fafarm \n5)Sobre \n6)Sair\n> '))
             if opr == 1:
                 cprint('AVISO : para funcionar corretamente o roblox precisa estar ABERTO', 'light_red')
                 print('Para parar o auto egg pressione q ')
@@ -31,15 +31,17 @@ def main():
                 t = float(input('especifique o tempo entre os clicks\n> '))
                 Basic_Utils.auto_click(t)
             if opr == 4:
-                print('Versão atual: 0.0.2\nBuild: Publica\ndata de lançamento: 21/07/2023')
+                Basic_Utils.event_farm()
+            if opr == 5:
+                print('Versão atual: 0.0.3\nBuild: Publica\ndata de lançamento: 22/07/2023')
                 cprint('Feito por : EXNOP#1293', 'light_magenta')
                 time.sleep(2)
                 main()
-            if opr == 5:
+            if opr == 6:
                 print('Saindo do programa :)')
         if lang == 2:
             print('Actual language : en')
-            opr = int(input('Select a  operation \n1)auto egg open \n2)afk \n3)auto farm \n4)about \n5)exit\n> '))
+            opr = int(input('Select a  operation \n1)auto egg open \n2)afk \n3)auto farm \n5)about \n6)exit\n> '))
             if opr == 1:
                 cprint('ATTENTION: to work correctly roblox needs to be OPEN', 'light_red')
                 print('to stop auto egg press q ')
@@ -52,10 +54,12 @@ def main():
                 t = float(input('specify time between clicks\n> '))
                 Basic_Utils.auto_click(t)
             if opr == 4:
-                print('Current version: 0.0.2\nBuild: Publica\nlaunch date: 21/07/2023')
+                Basic_Utils.event_farm()
+            if opr == 5:
+                print('Current version: 0.0.3\nBuild: Publica\nlaunch date: 22/07/2023')
                 cprint('made by : EXNOP#1293', 'light_magenta')
                 time.sleep(2)
                 main()
-            if opr == 5:
+            if opr == 6:
                 print('Exiting')
 main()
